d3ef5d630ea3e9e89e086958eab3bd3fc5212700
JohnRMora <100066599+JohnRMora@users.noreply.github.com>
2024-01-05 15:29:14 +0800

    Add files via upload

---
+++ Lab Activity 1/welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class welcome extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Welcome John Rubin Mora</h2>
+                
+            </div>
+        );
+    }
+}
+
+export default  welcome;
